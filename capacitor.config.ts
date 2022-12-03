c110be678fea6c8a01dada17daeed695873752bf
sys-walker <pere.rollon@gmail.com>
2022-12-03 14:18:13 +0100

    refactor: move to ionic framework

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'json-translations-editor-angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
