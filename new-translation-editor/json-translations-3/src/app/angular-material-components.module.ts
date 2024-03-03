b3d108d774301db57e0b791e8c2682bb74d7bfa3
sys-walker <pere.rollon@gmail.com>
2024-03-03 02:20:48 +0100

    add: design to file-tab

--- new-translation-editor/json-translations-3/src/app/angular-material-components.module.ts
+++ new-translation-editor/json-translations-3/src/app/angular-material-components.module.ts
@@ -1,34 +1,35 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule} from '@angular/material/card'
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {provideNativeDateAdapter} from '@angular/material/core';
-import {MatTabsModule} from '@angular/material/tabs';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
-  exports:[
+  imports: [CommonModule],
+  exports: [
+    MatListModule,
+    MatDividerModule,
     MatCardModule,
     MatToolbarModule,
     MatExpansionModule,
     MatButtonModule,
     MatExpansionModule,
     MatIconModule,
+    MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatTabsModule
+    MatTabsModule,
   ],
   providers: [provideNativeDateAdapter()],
-
 })
-export class AngularMaterialComponentsModule { }
+export class AngularMaterialComponentsModule {}
--- new-translation-editor/json-translations-3/src/app/components/editor-tabs/file-tab/file-tab.component.ts
+++ new-translation-editor/json-translations-3/src/app/components/editor-tabs/file-tab/file-tab.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './file-tab.component.scss'
 })
 export class FileTabComponent {
+  uploadFile(){
+    console.log("upload file clicked");
+    
+  }
 
 }
