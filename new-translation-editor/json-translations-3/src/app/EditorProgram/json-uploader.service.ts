4181c5e78d4d35b34df333fae9cf5ad9fd3f6c35
sys-walker <pere.rollon@gmail.com>
2024-03-16 23:19:57 +0100

    add FormatedJSON class

--- new-translation-editor/json-translations-3/src/app/EditorProgram/json-uploader.service.ts
+++ new-translation-editor/json-translations-3/src/app/EditorProgram/json-uploader.service.ts
@@ -1,20 +1,7 @@
 import { Injectable } from '@angular/core';
-import {
-  Observable,
-  Subscription,
-  catchError,
-  concatMap,
-  filter,
-  flatMap,
-  from,
-  map,
-  of,
-  switchMap,
-  tap,
-  throwError,
-  toArray,
-} from 'rxjs';
+import { from, of, switchMap } from 'rxjs';
 import { FilePicker, PickFilesResult, PickedFile } from '@capawesome/capacitor-file-picker';
+import { FormatedJSON } from '../util/json-formatter';
 
 @Injectable({
   providedIn: 'root',
@@ -40,17 +27,17 @@ export class JSONUploaderService {
               .map(async (object: PickedFile) => {
                 if (object.blob !== undefined) {
                   let filename = object.name;
-                  let stringifiedJSONJson = await object.blob.text();
+                  let stringifiedJSON = await object.blob.text();
 
-                  if (!stringifiedJSONJson) {
-                    stringifiedJSONJson = JSON.stringify({});
+                  if (!stringifiedJSON) {
+                    //show error????
+                    stringifiedJSON = JSON.stringify({});
                   }
-
-                  let parsedJSON = JSON.parse(stringifiedJSONJson);
+                  let objectJSON = JSON.parse(stringifiedJSON);
+                  let parsedJSON = FormatedJSON.flatten(objectJSON);
                   parsedJSON['langugeee'] = object.name;
                   return parsedJSON;
                 }
-
                 return undefined;
               })
               .filter((element) => {
@@ -60,117 +47,11 @@ export class JSONUploaderService {
 
           return r;
         })
-      )
-      .pipe
-      // switchMap((myArray) => {
-      //   const stringifiedJSON: string[] = myArray
-      //     .map((element) => (element === undefined ? '' : element)) // Replace undefined with empty strings
-      //     .filter((element) => element !== ''); // Filter out the empty strings
-
-      //   let parsed = stringifiedJSON.map((e) => {
-      //     console.log(JSON.parse(e));
-
-      //     return JSON.parse(e);
-      //   });
-
-      //   return of(parsed);
-      // })
-      ();
+      );
+    // .pipe(
+    //   switchMap((myArray) => {
+    //     return of(myArray);
+    //   })
+    // );
   }
-
-  // _uplaodFiles(): Observable<any[]> {
-  //   return from<Promise<PickFilesResult>>(
-  //     FilePicker.pickFiles({
-  //       types: ['application/json'],
-  //       multiple: true,
-  //     }).catch((err: Error) => {
-  //       console.log(err.message);
-  //       //Cancelled pick files
-  //       return { files: [] };
-  //     })
-  //   )
-  //     .pipe(switchMap((pickFiles: PickFilesResult) => of(pickFiles.files)))
-  //     .pipe(map(async (blobFileArr) => {
-
-  //       const jsonArray = await Promise.all(
-  //         blobFileArr
-  //           .map(async (object: any) => {
-  //             let e = object['blob'];
-  //             if (!e) {
-  //               return undefined;
-  //             }
-  //             return  await e.text();
-  //           })
-  //           .filter((element) => {
-  //             return element !== undefined && element !== null;
-  //           })
-  //       ).catch((err) => {
-  //         console.debug(err);
-  //         return undefined;
-  //       });
-  //       return of(jsonArray)
-  //     }));
-  // }
 }
-
-/*
-
-
-
-
-   catchError(() => of([])), // Handle cancellation or errors
-      map(files => files),
-      switchMap(fileArr => {
-        // Start loading indicator or handle loading state appropriately
-  
-        return from(fileArr.).pipe(
-          map(file => this.processFile(file)),
-          catchError(err => throwError(err)), // Propagate errors
-          filter(processedFile => processedFile !== undefined), // Remove skipped files
-          toArray() // Combine parsed files into a single array
-        );
-      })
-
-
-      //------------------------------------------------------------------------------
-  const pickFiles = await FilePicker.pickFiles({
-      types: ['application/json'],
-      multiple: true,
-    }).catch(() => {
-      return undefined;
-    });
-    if (pickFiles === undefined) {
-      //we did not pick files
-      return;
-    }
-
-    const fileArr = pickFiles.files;
-
-    //start loading
-
-    const blobFileArr = fileArr
-
-    const jsonArray = await Promise.all(
-      blobFileArr
-        .map(async (object: any) => {
-          let e = object['blob'];
-          if (!e) {
-            return undefined;
-          }
-          ret  await e.text();
-        })
-        .filter((element) => {
-          return element !== undefined && element !== null;
-        })
-    ).catch((err) => {
-      console.debug(err);
-      return undefined;
-    });
-    if (jsonArray === undefined) {
-      //alert
-      //Exit if something was wrong
-      return;
-    }
-    return jsonArray
-
-*/
